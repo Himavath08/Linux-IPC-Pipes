1bf82e182940bc30e0d23edf411a192be92d6554
Himavath <himavathm@gmail.com>
2024-05-06 14:27:20 +0530

    Update fifo1.c

--- fifo1.c
+++ fifo1.c
@@ -1,1 +1,30 @@
-//C Program that illustrate communication between two process using named pipes
+$ cat pipe1.c 
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h> 
+#include<sys/stat.h> 
+#include<string.h> 
+#include<fcntl.h> 
+#include<unistd.h>
+#include<sys/wait.h>
+void server(int,int); 
+void client(int,int); 
+int main() 
+{ 
+int p1[2],p2[2],pid, *waits; 
+pipe(p1); 
+pipe(p2); 
+pid=fork(); 
+if(pid==0) { 
+close(p1[1]); 
+close(p2[0]); 
+server(p1[0],p2[1]); return 0;
+ } 
+close(p1[0]); 
+close(p2[1]); 
+client(p1[1],p2[0]); 
+wait(waits); 
+return 0; 
+} 
+
+
